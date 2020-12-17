6f2633892712d0246c69498ad90f0167b04c62d7
Error-402 <64661285+Error-402@users.noreply.github.com>
2020-12-17 08:42:56 -0300

    Update Postagem_Bot.py

--- Postagem_Bot.py
+++ Postagem_Bot.py
@@ -1,6 +1,6 @@
 import facebook
 from Frases_Bot import frase
 
-token = 'EAAJMRMT1VI4BAKgGwh7ttjmMQRJRsbXdsvbHTsr9JhIY3HAMKDCxgW8oZAoGh8HjOZCwfZBWtBlFwz5vfS7HFaEkh9hB6ZAAfr8nc8E3ghuTUxZBh0093VK0Lz0CSUqCdNUkF2FlDFITW0ZBfPiymXAICTjy5ZC8DFIxSmplsYj2gZDZD'
+token = 'Token do Facebook'
 fb = facebook.GraphAPI(access_token=token)
 fb.put_object(parent_object='me', connection_name='feed',message=frase())
